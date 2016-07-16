01616aa1acf20c1565757b5497fdc31fc03b3a2b
Carlos Martin <carlos.martin@columbia.edu>
2016-07-16 12:56:55 -0400

    Refactored code, organized into classes

--- bandits.py
+++ bandits.py
@@ -1,3 +1,166 @@
+import numpy as np
+import scipy.stats
+import matplotlib.pyplot as plt
+import itertools
+
+# A (cheating) agent that chooses the action with the highest expected reward
+class OptimalAgent:
+	def __str__(self):
+		return 'Optimal agent'
+	def __init__(self, returns):
+		# The real expected returns are 'sneaked in'
+		self.returns = returns
+	def choose(self, actions):
+		return max(actions, key = lambda action: self.returns[action])
+	def receive(self, reward):
+		pass
+
+# An agent that chooses randomly between actions
+class RandomAgent:
+	def __str__(self):
+		return 'Random agent'
+	def choose(self, actions):
+		return np.random.choice(actions)
+	def receive(self, reward):
+		pass
+
+# An agent that chooses the action with the highest point estimate of the expected reward
+class GreedyAgent:
+	def __str__(self):
+		return 'Greedy agent'
+	def __init__(self):
+		self.action_history = []
+		self.reward_history = []
+	def choose(self, actions):
+		# Point estimates for the expected reward of each action
+		totals = {action: 0 for action in actions}
+		counts = {action: 0 for action in actions}
+		for action, reward in zip(self.action_history, self.reward_history):
+			totals[action] += reward
+			counts[action] += 1
+		estimates = {action: totals[action] / (counts[action] + 1) for action in actions}
+		action = max(actions, key = lambda action: estimates[action])
+		self.action_history.append(action)
+		return action
+	def receive(self, reward):
+		self.reward_history.append(reward)
+
+# An agent that chooses an action according to the probability that it maximizes the expected reward
+class ThompsonAgent:
+	def __str__(self):
+		return 'Thompson agent'
+	def __init__(self):
+		self.action_history = []
+		self.reward_history = []
+	def choose(self, actions):
+		successes = {action: 0 for action in actions}
+		failures = {action: 0 for action in actions}
+		for action, reward in zip(self.action_history, self.reward_history):
+			if reward == 1:
+				successes[action] += 1
+			elif reward == 0:
+				failures[action] += 1
+		estimates = {action: scipy.stats.beta.rvs(successes[action] + 1, failures[action] + 1) for action in actions}
+		action = max(actions, key = lambda action: estimates[action])
+		self.action_history.append(action)
+		return action
+	def receive(self, reward):
+		self.reward_history.append(reward)
+
+# List of all possible actions
+number_of_actions = 10
+actions = [scipy.stats.bernoulli(scipy.stats.uniform().rvs()) for n in range(number_of_actions)]
+
+# Expected returns for each action
+returns = {action: action.expect() for action in actions}
+print('Expected returns: ' + ', '.join('{:.2f}'.format(value) for value in returns.values()))
+
+# List of agents
+agents = [OptimalAgent(returns), RandomAgent(), GreedyAgent(), ThompsonAgent()]
+
+# History of rewards received by each agent
+rewards = {}
+for agent in agents:
+	rewards[agent] = []
+
+# Simulation
+number_of_rounds = 1000
+for rounds in range(number_of_rounds):
+	for agent in agents:
+
+		# Find action chosen by agent
+		choice = agent.choose(actions)
+
+		# Find reward returned by chosen action
+		reward = choice.rvs()
+
+		# Reward agent for chosen action
+		agent.receive(reward)
+
+		# Add reward to history of rewards for this agent
+		rewards[agent].append(reward)
+
+plt.title('Multi-armed bandit problem with ' + str(number_of_actions) + ' arms')
+for agent in agents:
+	plt.plot(list(itertools.accumulate(rewards[agent])), label = str(agent))
+plt.xlabel('Rounds')
+plt.ylabel('Cumulative reward')
+plt.legend()
+plt.show()
+
+
+
+
+
+
+
+
+exit()
+
+
+import numpy as np
+
+class Agent:
+	def __init__(self):
+		self.rewards = []
+
+	def choose(self, actions):
+		raise NotImplementedError
+
+	def receive(self, reward):
+		raise NotImplementedError
+
+
+
+
+
+class RandomAgent(Agent):
+	def choose(self, actions):
+		return np.random.choice(actions)
+
+
+
+
+
+
+agent = RandomAgent()
+
+print(agent.choose(['move left', 'move right']))
+
+
+
+
+
+
+
+
+
+
+
+exit()
+
+
+
 import scipy.stats
 import numpy as np
 import matplotlib.pyplot as plt
@@ -47,8 +210,9 @@ while True:
 			histories[agent].append((choice, choice.rvs()))
 			if step % skip == 0: plt.plot(tuple(itertools.accumulate([event[1] for event in histories[agent]])), color = 'blue')
 		elif agent == 'semi-greedy':
-			greediness = np.exp(-len(histories))
-			if scipy.stats.uniform.rvs() > greediness:
+			exploration_probability = np.exp(-len(histories) / arms)
+			if scipy.stats.uniform.rvs() < exploration_probability:
+				# Perform greedy strategy
 				history = histories[agent]
 				estimates = {}
 				for distribution in distributions:
